aebe73e02ce55788a5ac7bb41d8299352b316751
Dan <rodzi1988@gmail.com>
2015-08-09 13:02:29 -0700

    Requests should now pull json (better support via libraries) Need to spin up linux box for testing due to SSL handshake error on windows

--- API.py
+++ API.py
@@ -1,28 +1,8 @@
-import urllib2
+
 import requests
-from xml.dom import minidom
-from xml.etree import ElementTree
-response = urllib2.urlopen("https://search.lib.byu.edu/green/byu/record/lee.383397").read()
-#elements = ElementTree.fromstring(response)
-#print elements
+import json
 
-#response = requests.get("https://search.lib.byu.edu/green/byu/record/lee.383397", stream=True)
-# if the server sent a Gzip or Deflate compressed response, decompress
-# as we read the raw stream:
-#xmldoc = minidom.parse(response)
-e = ElementTree.fromstring(response)
-print e
 
-#for hold in e.find
-
-#for element in xmldoc.getElementsByTagName('marcHoldings'):
-#    marcHolding = element.getElementsByTagName('marcHolding')
-#    spans = marcHolding.getElementsByTagName('spans')
-#    print spans
-
-#response.raw.decode_content = True
-
-#events = ElementTree.iterparse(response.raw)
-#for elem, event in events:
-    # do something with `elem`
-#    print elem
+response = requests.get("https://search.lib.byu.edu/green/byu/record/lee.383397", verify=False)
+obj = json.loads(response.text)
+print obj
