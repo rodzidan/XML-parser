7a11dc3e717d2105b9bb0760459a256fefb79ef7
dan <dan@localhost.localdomain>
2015-08-09 15:40:52 -0600

    Function for all three urls working correctly. Now need to spin up server and build as service.

--- API.py
+++ API.py
@@ -5,7 +5,25 @@ import requests
 import json
 
 
+# function which finds the start year from a list of spans, given that some spans may not conatin a year
+# @param spans - the list of spans
+# @return firstYear the first year for the span
+def getFirstYearFromSpans(spans):
+	firstYear = 0
+	# iterate over the spans 
+	for span in spans:
+		# check for a year key in the start element
+		# if the year key is present use it store the start date
+		if 'year' in span['start']:
+			firstYear = span['start']['year']
+			break	
+	# return the start date 
+	return firstYear
 
+
+# function which returns the run for a given holding via its URI
+# @param uri - the uri for the holdings
+# @return Array - array containing the start and end year for the holding
 def getHoldings(uri):
 	# Make http request and then assign reposne to var response.
 	# Code will not continue executino until response is saved
@@ -14,9 +32,13 @@ def getHoldings(uri):
 	# jsonResponse - the json returned from the http request as a json Object 
 	jsonResponse = json.loads(response.text)
 
-	# get the first year 
-	firstYear = jsonResponse['holdings'][0]['marcHoldings'][0]['spans'][0]['start']['year']
-
+	# get all the spans and find the first span with a start year
+	
+	spans = jsonResponse['holdings'][0]['marcHoldings'][0]['spans']
+	
+	# find the first year 
+	
+	firstYear = getFirstYearFromSpans(spans)	
 
 	#get the latest year
 
