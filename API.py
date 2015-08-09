a23aedca4e60c52881ff4b4d21225b23fc9443b6
dan <dan.mark.rodziewicz@gmail.com>
2015-08-09 13:11:07 -0700

    Better variable names and comments

--- API.py
+++ API.py
@@ -1,8 +1,13 @@
-
+# imports 
+# requests - library for http requests
 import requests
+# json - library to create and parse json objects
 import json
 
-
+# Make http request and then assign reposne to var response.
+# Code will not continue executino until response is saved
 response = requests.get("https://search.lib.byu.edu/green/byu/record/lee.383397.json", verify=False)
-obj = json.loads(response.text)
-print obj
+
+# jsonResponse - the json returned from the http request as a json Object 
+jsonResponse = json.loads(response.text)
+print jsonResponse
